1df7614c5ba5f1b94ddc3d4b4d71520de1c9f42b
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-06-29 18:19:56 +0530

    Projects page done

--- controllers/main.js
+++ controllers/main.js
@@ -11,13 +11,13 @@ exports.about = async (request, response, next) => {
     const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
     // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
     // console.log(jsonResponse);
-    response.render("main/index", {title: 'About'});
+    response.render("main/about", {title: 'About'});
 };
 
 exports.projects = async (request, response, next) => {
     const [sheetName, authToken] = ['Projects', process.env.AUTH_TOKEN];
-    const jsonResponse = await fetchData({ sheetName, authToken });
-    console.log(jsonResponse);
+    // const jsonResponse = await fetchData({ sheetName, authToken });
+    // console.log(jsonResponse);
     response.render("main/projects", {title: 'Projects'});
 };
 
@@ -25,14 +25,21 @@ exports.members = async (request, response, next) => {
     const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
     // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
     // console.log(jsonResponse);
-    response.render("main/index", {title: 'Members'});
+    response.render("main/members", {title: 'Members'});
+};
+
+exports.gallery = async (request, response, next) => {
+    const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
+    // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
+    // console.log(jsonResponse);
+    response.render("main/gallery", {title: 'Gallery'});
 };
 
 exports.contact = async (request, response, next) => {
     const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
     // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
     // console.log(jsonResponse);
-    response.render("main/index", {title: 'Contact'});
+    response.render("main/contact", {title: 'Contact'});
 };
 
 exports.temp = (request, response, next) => {
--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { home, about, projects, members, contact, temp } = require('../controllers/main');
+const { home, about, projects, members, gallery, contact, temp } = require('../controllers/main');
 
 router.route('/').get(home);
 router.route('/about').get(about);
 router.route('/projects').get(projects);
 router.route('/members').get(members);
+router.route('/gallery').get(gallery);
 router.route('/contact').get(contact);
 router.route('/temp').get(temp);
 // router.route('/blog').get(blog);
