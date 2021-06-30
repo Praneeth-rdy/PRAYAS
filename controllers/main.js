5d37d372c867ecf051632b92d7a42b3b79f86688
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-06-30 17:26:25 +0530

    Blog routes configured and navbar updated

--- controllers/main.js
+++ controllers/main.js
@@ -4,44 +4,54 @@ exports.home = async (request, response, next) => {
     const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
     // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
     // console.log(jsonResponse);
-    response.render("main/index", {title: 'Home'});
+    response.render("main/index", { title: 'Home' });
 };
 
 exports.about = async (request, response, next) => {
     const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
     // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
     // console.log(jsonResponse);
-    response.render("main/about", {title: 'About'});
+    response.render("main/about", { title: 'About' });
 };
 
 exports.projects = async (request, response, next) => {
     const [sheetName, authToken] = ['Projects', process.env.AUTH_TOKEN];
     // const jsonResponse = await fetchData({ sheetName, authToken });
     // console.log(jsonResponse);
-    response.render("main/projects", {title: 'Projects'});
+    response.render("main/projects", { title: 'Projects' });
 };
 
 exports.members = async (request, response, next) => {
     const [sheetName, authToken] = ['Members', process.env.AUTH_TOKEN];
     const jsonResponse = await fetchData({ sheetName, authToken });
     console.log(jsonResponse);
-    response.render("main/members", {title: 'Members', data: jsonResponse.data});
+    response.render("main/members", { title: 'Members', data: jsonResponse.data });
 };
 
 exports.gallery = async (request, response, next) => {
     const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
     // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
     // console.log(jsonResponse);
-    response.render("main/gallery", {title: 'Gallery'});
+    response.render("main/gallery", { title: 'Gallery' });
+};
+
+exports.blog = async (request, response, next) => {
+    const { id } = request.params;
+    if (id) {
+        response.render("main/blog", { title: 'Blog' });
+    } else {
+        response.render("main/blogs_list", { title: 'Blog' });
+    }
+
 };
 
 exports.contact = async (request, response, next) => {
     const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
     // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
     // console.log(jsonResponse);
-    response.render("main/contact", {title: 'Contact'});
+    response.render("main/contact", { title: 'Contact' });
 };
 
 exports.temp = (request, response, next) => {
-    response.render('temp', {title: 'Demo', books: ['book1', 'book2', 'book3']})
+    response.render('temp', { title: 'Demo', books: ['book1', 'book2', 'book3'] })
 }
--- routes/main.js
+++ routes/main.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { home, about, projects, members, gallery, contact, temp } = require('../controllers/main');
+const { home, about, projects, members, gallery, blog, contact, temp } = require('../controllers/main');
 
 router.route('/').get(home);
 router.route('/about').get(about);
 router.route('/projects').get(projects);
 router.route('/members').get(members);
 router.route('/gallery').get(gallery);
+router.route('/blog/').get(blog);
+router.route('/blog/:id/').get(blog);
 router.route('/contact').get(contact);
 router.route('/temp').get(temp);
 // router.route('/blog').get(blog);
