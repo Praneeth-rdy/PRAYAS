aa5e043e90b67e81fca3c767b9ffca2a86af57c0
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-08-11 22:08:57 +0530

    format set

--- controllers/main.js
+++ controllers/main.js
@@ -6,92 +6,93 @@ const Image = models.Image;
 const fetchData = require('../utils/fetchData');
 
 exports.home = async (request, response, next) => {
-    const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
-    // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
-    // console.log(jsonResponse);
-    response.render("main/index", { title: 'Home' });
+  const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
+  // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
+  // console.log(jsonResponse);
+  response.render("main/index", { title: 'Home' });
 };
 
 exports.about = async (request, response, next) => {
-    const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
-    // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
-    // console.log(jsonResponse);
-    response.render("main/about", { title: 'About' });
+  const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
+  // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
+  // console.log(jsonResponse);
+  response.render("main/about", { title: 'About' });
 };
 
 exports.projects = async (request, response, next) => {
-    const [sheetName, authToken] = ['Projects', process.env.AUTH_TOKEN];
-    // const jsonResponse = await fetchData({ sheetName, authToken });
-    // console.log(jsonResponse);
-    response.render("main/projects", { title: 'Projects' });
+  const [sheetName, authToken] = ['Projects', process.env.AUTH_TOKEN];
+  // const jsonResponse = await fetchData({ sheetName, authToken });
+  // console.log(jsonResponse);
+  response.render("main/projects", { title: 'Projects' });
 };
 
 exports.members = async (request, response, next) => {
-    const [sheetName, authToken] = ['Members', process.env.AUTH_TOKEN];
-    const jsonResponse = await fetchData({ sheetName, authToken });
-    console.log(jsonResponse);
-    response.render("main/members", { title: 'Members', data: jsonResponse.data });
+  const [sheetName, authToken] = ['Members', process.env.AUTH_TOKEN];
+  const jsonResponse = await fetchData({ sheetName, authToken });
+  console.log(jsonResponse);
+  response.render("main/members", { title: 'Members', data: jsonResponse.data });
 };
 
 exports.gallery = async (request, response, next) => {
-    const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
-    // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
-    // console.log(jsonResponse);
-    response.render("main/gallery", { title: 'Gallery' });
+  const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
+  // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
+  // console.log(jsonResponse);
+  response.render("main/gallery", { title: 'Gallery' });
 };
 
 exports.blog = async (request, response, next) => {
-    const { blogId } = request.params;
-    if (blogId) {
-        response.render("main/blog", { title: 'Blog' });
-    } else {
-        response.render("main/blogs_list", { title: 'Blog' });
-    }
+  const { blogId } = request.params;
+  if (blogId) {
+    response.render("main/blog", { title: 'Blog' });
+  } else {
+    response.render("main/blogs_list", { title: 'Blog' });
+  }
 
 };
 
 exports.contact = async (request, response, next) => {
-    const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
-    // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
-    // console.log(jsonResponse);
-    response.render("main/contact", { title: 'Contact' });
+  const [sheetName, authToken, isVertical] = ['HomeVars', process.env.AUTH_TOKEN, true];
+  // const jsonResponse = await fetchData({ sheetName, authToken, isVertical });
+  // console.log(jsonResponse);
+  response.render("main/contact", { title: 'Contact' });
 };
 
 exports.temp = (request, response, next) => {
-    response.render('temp', { title: 'Demo', books: ['book1', 'book2', 'book3'] })
+  response.render('temp', { title: 'Demo', books: ['book1', 'book2', 'book3'] })
 }
 
 exports.upload = async (request, response, next) => {
-    if(request.method == 'POST') {
-        try {
-            console.log(request.files);
-            // response.send({
-            //     'a':'b'
-            // });
-        
-            if (request.files == undefined) {
-              return response.send(`You must select a file.`);
-            }
+  if (request.method == 'POST') {
+    try {
+      console.log(request.files);
+      // response.send({
+      //     'a':'b'
+      // });
 
-            Image.create({
-              type: request.files[0].mimetype,
-              name: request.files[0].originalname,
-              data: fs.readFileSync(
-                __basedir + "/public/media/tmp/" + request.files[0].filename
-              ),
-            }).then((image) => {
-              fs.writeFileSync(
-                __basedir + "/public/media/" + image.name,
-                image.data
-              );
-        
-              return response.send(`File has been uploaded.`);
-            });
-          } catch (error) {
-            console.log(error);
-            return response.send(`Error when trying upload images: ${error}`);
-          }
-    } else {
-        response.render('upload', { title: 'Upload' });
+      if (request.files == undefined) {
+        return response.send(`You must select a file.`);
+      }
+
+      Image.create({
+        type: request.files[0].mimetype,
+        name: request.files[0].originalname,
+        data: "/media/" + request.files[0].originalname
+      }).then((image) => {
+        fs.writeFileSync(
+          __basedir + "/public/media/" + image.name, fs.readFileSync(
+            __basedir + "/public/media/tmp/" + request.files[0].filename
+          )
+        );
+        // delete
+        fs.unlinkSync(__basedir + "/public/media/tmp/" + request.files[0].filename);
+        return response.send(`File has been uploaded.`);
+      });
+    } catch (error) {
+      console.log(error);
+      return response.send(`Error when trying upload images: ${error}`);
     }
+  } else {
+    const images = await Image.findAll();
+    response.render('upload', { title: 'Upload', images });
+  }
 }
